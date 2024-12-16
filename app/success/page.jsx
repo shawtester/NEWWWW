dc62c2998de0759cff7393305fadbde8c72c1bf7
shawtester <96066025+shawtester@users.noreply.github.com>
2024-12-17 05:18:06 +0530

    first commit

--- app/success/page.jsx
+++ app/success/page.jsx
@@ -1,26 +1,9 @@
-"use client";
+import React from 'react'
 
-import { useRouter } from "next/navigation";
-
-const SuccessPage = () => {
-  const router = useRouter();
-  
-  // Destructure the query parameters from the URL
-  const { transactionId, amount, providerReferenceId, status, message } = router.query;
-
-  if (!transactionId || !amount || !providerReferenceId || !status || !message) {
-    return <p>Loading...</p>; // Show loading until data is available
-  }
-
+const page = () => {
   return (
-    <div>
-      <h1>Payment Status: {status === 'completed' ? 'Success' : 'Failure'}</h1>
-      <p><strong>Transaction ID:</strong> {transactionId}</p>
-      <p><strong>Amount:</strong> {amount}</p>
-      <p><strong>Provider Reference ID:</strong> {providerReferenceId}</p>
-      <p><strong>Message:</strong> {decodeURIComponent(message)}</p>
-    </div>
-  );
-};
+    <div>page</div>
+  )
+}
 
-export default SuccessPage;
+export default page
